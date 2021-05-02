9b7d74f257f38bb2e8ee0aad308fcf02ed1bdad5
mks51 <70435639+mks51@users.noreply.github.com>
2021-05-02 17:54:12 +0530

    body-parser added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+require('dotenv').config();
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const app = express();
 
