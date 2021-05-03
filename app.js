f190ce386baeac366d5487f8ee827018a2245276
irfanaliansari35 <irfanaliansari35@gmail.com>
2021-05-03 12:53:34 +0530

    added ejs
    #added ejs
    #link stylesheet
    #public folder

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const { static } = require('express');
 
+const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-const app = express();
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-    res.send("Server is up and running.");
+    res.sendFile(__dirname+"/index.html");
 })
 
 
