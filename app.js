3e839e48c5fd2d129f7171bd434eb850689294ef
irfanaliansari35 <irfanaliansari35@gmail.com>
2021-05-07 02:06:12 +0530

    medicine added
    add lead form
    medicine page
    cases colour changes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const axios = require('axios');
+const { inflateRawSync } = require('zlib');
 
 const app = express();
 
@@ -13,6 +15,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const storeSchema = {
+  name : String,
+  address : String,
+  contact : Number,
+  medicine : String,
+  district : String
+};
+
+const Store = new mongoose.model("Store",storeSchema);
 
 
 app.get("/", function(req,res){
@@ -53,11 +67,33 @@ app.get("/vaccination", function(req,res){
   res.render("vaccination");
   
 });
+app.get("/medicines",function(req,res){
 
+  Store.find({},function(err,foundItems){
+      res.render("medicines",{newListItems: foundItems});
+  })
 
+  
+});
 
+app.post("/addlead",function(req,res){
+  const storeName = req.body.storeName;
+  const storeDistrict = req.body.district;
+  const storeContact = req.body.contact;
+  const storeAddress = req.body.contact;
+  const storeMedicine = req.body.medicine;
 
+  console.log(storeName);
+  console.log(storeDistrict);
+  console.log(storeContact);
+  console.log(storeAddress);
+  console.log(storeMedicine);
 
+  res.redirect("/medicines");
+
+})
+
+  
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
