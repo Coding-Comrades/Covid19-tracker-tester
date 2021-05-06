389dfd5317bf256120870189a44e2b818f9f6ce8
hvs42 <harshvardhansingh2403@gmail.com>
2021-05-06 18:29:03 +0530

    #Api Calling Done #program for Dynamic table creation done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const { static } = require('express');
+const axios = require('axios');
 
 const app = express();
 
@@ -14,11 +14,34 @@ app.use(express.static("public"));
 
 
 
+
 app.get("/", function(req,res){
-    res.render("home");
+
+  axios.get('https://api.covid19india.org/data.json')
+    .then(response => {
+      // console.log(result.data);
+      const data = response.data;
+      const statewiseDatas = data.statewise;
+      const cases = data.cases_time_series;
+      const testedDatas = data.tested;
+      // console.log(statewiseData);
+      // console.log(cases[cases.length - 1]);
+      // console.log(testedDatas[testedData.length - 1]);
+      console.log("api called");
+      res.render("home", {
+        currentCase : cases[cases.length - 1],
+        statewiseData : statewiseDatas,
+        testData : testedDatas[testedDatas.length - 1]
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  
+    
     
 });
-
 
 
 app.get("/about", function(req,res){
